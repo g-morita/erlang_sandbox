3cae6e49bdd2c0c12f4a3e88bea695cf3b68a2b1
g-morita <g-morita@ec-one.com>
2011-09-30 10:55:44 +0900

    indent

--- ex5/frequency3.erl
+++ ex5/frequency3.erl
@@ -2,7 +2,6 @@
 -export([start/0, stop/0, allocate/0, deallocate/1]).
 -export([init/0, get_frequencies/0, loop/1, call/1, reply/2, allocate/2, deallocate/3, count/2, count/3]).
 
-
 start() ->
   register(?MODULE, spawn(?MODULE, init, [])).
 
@@ -25,22 +24,22 @@ get_frequencies() ->
 % ループデータ
 loop(Frequencies) ->
   receive
-	  {request, Pid, allocate} ->
-		  {NewFrequencies, Reply} = allocate(Frequencies, Pid),
-	    reply(Pid, Reply),
-			loop(NewFrequencies);
-	  {request, Pid, {deallocate, Freq}} ->
-		  {NewFrequencies, Reply} = deallocate(Frequencies, Freq, Pid),
-	    reply(Pid, Reply),
-			loop(NewFrequencies);
-	  {request, Pid, stop} ->
-			case Frequencies of
-			  {[], _} ->
-    	    reply(Pid, ok);
-				_ -> 
-    	    reply(Pid, error),
-					loop(Frequencies)
-			end
+    {request, Pid, allocate} ->
+      {NewFrequencies, Reply} = allocate(Frequencies, Pid),
+      reply(Pid, Reply),
+      loop(NewFrequencies);
+    {request, Pid, {deallocate, Freq}} ->
+      {NewFrequencies, Reply} = deallocate(Frequencies, Freq, Pid),
+      reply(Pid, Reply),
+      loop(NewFrequencies);
+    {request, Pid, stop} ->
+      case Frequencies of
+        {[], _} ->
+          reply(Pid, ok);
+        _ -> 
+          reply(Pid, error),
+          loop(Frequencies)
+    end
   end.
 
 allocate({[], Allocated}, _Pid) ->
@@ -48,21 +47,21 @@ allocate({[], Allocated}, _Pid) ->
 allocate({[Freq|Free], Allocated}, Pid) ->
   io:format("count:~p~n", [count(Allocated, Pid)]),
   case count(Allocated, Pid) < 3 of
-	  true ->
+    true ->
       { {Free, [{Freq,Pid}|Allocated] }, {ok, Freq}};
-		_ ->
-		  {{Free, Allocated}, allocate_over_error}
-	end.
+    _ ->
+      {{Free, Allocated}, allocate_over_error}
+  end.
 
 deallocate({Free, Allocated}, Freq, Pid) ->
   % list:member関数を利用する. lists:memberはガードに使えない!?
-	case lists:member({Freq,Pid}, Allocated) of
-	  true -> 
+  case lists:member({Freq,Pid}, Allocated) of
+    true -> 
       NewAllocated = lists:keydelete(Freq, 1, Allocated),
-	    {{[Freq|Free], NewAllocated}, ok};
+      {{[Freq|Free], NewAllocated}, ok};
     _ ->
       {{Free, Allocated}, deallocate_error}
-	end.
+  end.
 
 call(Msg) ->
   ?MODULE ! {request, self(), Msg},
@@ -82,8 +81,8 @@ count([], _Pid, Count) ->
   Count;
 count([Head|Tail], Pid, Count) ->
   case Head of
-	  {_, Pid} ->
-		  count(Tail, Pid, Count + 1);
-		_ ->
-		  count(Tail, Pid, Count)
-	end.
+    {_, Pid} ->
+      count(Tail, Pid, Count + 1);
+    _ ->
+      count(Tail, Pid, Count)
+  end.
